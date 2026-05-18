66bcada9105f2bfc325e3da17e18e079ad3306ff
JordanChevalier <jordanchevalier33@orange.fr>
2026-05-18 15:18:58 +0200

    Connection serveur /  client

--- competition.sh
+++ competition.sh
@@ -1,17 +1,19 @@
 PORT=$(python -c "import random; print(random.randint(50000, 60000))")
 echo "La compétition se passe sur 127.0.0.1:$PORT"
+#uvx --from amazinggame killall
 
-uvx --from amazinggame killall
 find . -name "*.log" -exec rm \{} \;
 
 uvx --from amazinggame server -p $PORT --timeout 5 &
+
 uvx --from amazinggame viewer -p $PORT &
 # https://www.speedscope.app/
 # py-spy record -o profile.data --format speedscope -- python -m amazinggame.viewer -p $PORT &
 #uv run python -m amazinggame.viewer -p $PORT --small-window &
 sleep 2
 
-uv run python /home/vincent/Documents/programmation/amazing_player/sample_player_client.py -p $PORT -u "Flash" --winner &
+gcc -Wall -o lesfouduvolant.exe sample_player_client.c
+./lesfouduvolant.exe 127.0.0.1 $PORT "LesFousDuVolant"
 
 sleep 500
-uvx --from amazinggame killall
+#uvx --from amazinggame killall
